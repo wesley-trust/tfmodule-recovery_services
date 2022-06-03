ccaecf42754d4f08b703ad38ddf447c09db1afc8
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2022-06-03 12:09:49 +0100

    Update resource

--- resources_time.tf
+++ resources_time.tf
@@ -1,6 +1,5 @@
 # Delay destroy by 30 seconds, to allow for service replication
 resource "time_sleep" "wait_30_seconds" {
-  depends_on = [azurerm_recovery_services_vault.recovery_services_vault]
 
   destroy_duration = "30s"
 }
