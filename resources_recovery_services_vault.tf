2129e78298d61a6ad5009b64c52cb0f7bc30b80a
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2022-06-03 13:50:17 +0100

    Update

--- resources_recovery_services_vault.tf
+++ resources_recovery_services_vault.tf
@@ -1,8 +1,5 @@
 # Recovery services vault for solution
 resource "azurerm_recovery_services_vault" "recovery_services_vault" {
-  depends_on = [
-    time_sleep.wait_30_seconds
-  ]
   name                = "${local.resource_name}-vault"
   location            = module.resource_group.location
   resource_group_name = module.resource_group.name
--- resources_time.tf
+++ resources_time.tf
@@ -1,5 +1,6 @@
-# Delay destroy by 30 seconds, to allow for service replication
+/* # Delay destroy by 30 seconds, to allow for service replication
 resource "time_sleep" "wait_30_seconds" {
 
   destroy_duration = "30s"
 }
+ */
