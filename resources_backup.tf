a2427acf18e8a5b9a9997b6f444e90bc327c567d
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2022-06-03 11:02:08 +0100

    Correct reference

--- resources_backup.tf
+++ resources_backup.tf
@@ -39,7 +39,7 @@ resource "azurerm_backup_policy_vm" "backup_policy_vm" {
 resource "azurerm_backup_protected_vm" "backup_protected_vm" {
   depends_on = [
     azurerm_backup_policy_vm.backup_policy_vm,
-    azurerm_recovery_services_vault
+    azurerm_recovery_services_vault.recovery_services_vault
   ]
   count               = var.resource_recovery_services_instance_count
   resource_group_name = module.resource_group.name
